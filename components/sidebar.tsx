ad3aed536fd6b89958321f861bf16404afb6fbd8
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-12-13 11:53:10 -0600

    Added logo to sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -22,7 +22,7 @@ const Sidebar = () => {
         <Box width="100%" height="calc(100vh - 100px)" bg="black" paddingX="5px" color="gray">
             <Box paddingY="20px">
                 <Box width="120px" marginBottom="20px" paddingX="20px">
-
+                    <NextImage src="/logo.svg" height={60} width={120}/>
                 </Box>
             </Box>
         </Box>
