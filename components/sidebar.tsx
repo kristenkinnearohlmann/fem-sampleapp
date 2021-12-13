b2999f5a453de1551a06bfffe9ebc414b09d1ed8
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-12-13 11:43:51 -0600

    Added react icons to sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -8,6 +8,13 @@ import {
     LinkBox,
     LinkOverlay
 } from '@chakra-ui/layout'
+import {
+    MdHome,
+    MdSearch,
+    MdLibraryMusic,
+    MdPlaylistAdd,
+    MdFavorite
+} from 'react-icons/md'
 
 const Sidebar = () => {
     return (
