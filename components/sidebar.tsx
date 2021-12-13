e04a9a72d893f8167e15ebce34eff2400e8238a4
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-12-13 13:39:18 -0600

    Fixed content box in nav to be 100 pct

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -52,7 +52,7 @@ const musicMenu = [
 const Sidebar = () => {
     return (
         <Box width="100%" height="calc(100vh - 100px)" bg="black" paddingX="5px" color="gray">
-            <Box paddingY="20px">
+            <Box paddingY="20px" height="100%">
                 <Box width="120px" marginBottom="20px" paddingX="20px">
                     <NextImage src="/logo.svg" height={60} width={120} />
                 </Box>
