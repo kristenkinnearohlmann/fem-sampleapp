18c8a3841258a7f0f71c233e83ddbe835afce3fe
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-12-13 14:01:18 -0600

    Added Prisma

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -91,7 +91,7 @@ const Sidebar = () => {
                     </List>
                 </Box>
                 <Divider color="gray.800" />
-                <Box height="66%" overflowY="auto" paddingY="20px">
+                <Box height="55%" overflowY="auto" paddingY="20px">
                     <List spacing={2}>
                         {playlists.map((playlist) => (
                         <ListItem paddingX="20px" key={playlist}>
