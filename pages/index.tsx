0cf8373c84b870e33a91eb5f0f31216104393ae7
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-12-14 13:28:59 -0600

    Initial home component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,14 @@
 import Head from "next/head";
 import Image from "next/image";
+import GradientLayout from "../components/gradientLayout";
 import styles from "../styles/Home.module.css";
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
-  )
-}
+    <GradientLayout color="red">
+      <div>Home page</div>
+    </GradientLayout>
+  );
+};
 
 export default Home;
