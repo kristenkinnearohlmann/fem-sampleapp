dffd70cb418e97923a21726a9cc6cda0d0dfb766
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-12-14 14:06:28 -0600

    Artist cards not working, compare point

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
+import { NextPageContext } from "next";
 import Head from "next/head";
-import Image from "next/image";
 import GradientLayout from "../components/gradientLayout";
 import styles from "../styles/Home.module.css";
+import prisma from "../lib/prisma";
+import { Box, Flex, Text } from "@chakra-ui/layout";
+import { Image } from "@chakra-ui/react";
 
-const Home = () => {
+const Home = ({ artists }) => {
+  console.log(artists);
   return (
     <GradientLayout
       color="orange"
@@ -13,9 +17,40 @@ const Home = () => {
       image="https://tinted-gym-f99.notion.site/image/https%3A%2F%2Fdl.dropboxusercontent.com%2Fs%2Fbgiv0ssz3xpotz9%2Fpeep.png%3Fdl%3D0?table=block&id=33f9771b-0e6f-4a72-832c-69ed2d41f290&spaceId=511cd811-5561-4a61-b550-c4086b4afafb&width=2000&userId=&cache=v2"
       roundImage
     >
-      <div>Home page</div>
+      <Box color="white" paddingX="40px">
+        <Box marginBottom="20px">
+          <Text fontSize="2xl" fontWeight="bold">
+            Top artists this month
+          </Text>
+          <Text fontSize="md">only visible to you</Text>
+        </Box>
+        <Flex justify="space-between">
+          {artists.map((artist) => {
+            <Box paddingX="10px">
+              <Box bg="gray.900" borderRadius="4px" padding="15px" width="100%">
+                <Image
+                  src="https://placekitten.com/200/300"
+                  borderRadius="100%"
+                />
+                <Box>
+                  <Text>{artist.name}</Text>
+                  <Text>Artist</Text>
+                </Box>
+              </Box>
+            </Box>;
+          })}
+        </Flex>
+      </Box>
     </GradientLayout>
   );
+};
+
+export const getServerSideProps = async () => {
+  const artists = await prisma.artist.findMany();
+  console.log(artists);
+  return {
+    props: { artists },
+  };
 };
 
 export default Home;
