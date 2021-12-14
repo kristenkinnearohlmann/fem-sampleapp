91531dcf9f661838ec6e3c0a727d33ffe190737c
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-12-14 15:22:46 -0600

    Added playlist view

--- pages/playlist/[id].tsx
+++ pages/playlist/[id].tsx
@@ -1,8 +1,35 @@
+import GradientLayout from "../../components/gradientLayout";
 import { validateToken } from "../../lib/auth";
 import prisma from "../../lib/prisma";
 
+const getBGColor = (id) => {
+  const colors = [
+    "red",
+    "green",
+    "blue",
+    "orange",
+    "purple",
+    "gray",
+    "teal",
+    "yellow",
+  ];
+  return colors[id - 1] || colors[Math.floor(Math.random() * colors.length)];
+};
+
 const Playlist = ({ playlist }) => {
-  return <div>{playlist.name}</div>;
+  const color = getBGColor(playlist.id);
+  return (
+    <GradientLayout
+      color={color}
+      title={playlist.name}
+      subtitle="Playlist"
+      description={`${playlist.songs.length} songs`}
+      roundImage={false}
+      image={`https://picsum.photos/400?random=${playlist.id}`}
+    >
+      <div>hello</div>
+    </GradientLayout>
+  );
 };
 
 export const getServerSideProps = async ({ query, req }) => {
