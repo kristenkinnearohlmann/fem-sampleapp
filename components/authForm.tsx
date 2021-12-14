5ad89ac17f0b64751b1081898feff3cae4350429
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-12-14 10:52:56 -0600

    Completed signup and signin

--- components/authForm.tsx
+++ components/authForm.tsx
@@ -3,6 +3,7 @@ import { useRouter } from "next/router";
 import { useSWRConfig } from "swr";
 import { auth } from "../lib/mutations";
 import { FC, useState } from "react";
+import NextImage from "next/image";
 
 // check Scott's solution for Typescript checking component
 
@@ -12,13 +13,44 @@ const AuthForm = ({ mode }) => {
   const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    const user = await auth(mode, { email, password });
+    setIsLoading(false);
+    router.push("/");
+  };
+
   return (
     <Box height="100vh" width="100vw" bg="black" color="white">
-      <Flex justify="center" align="center" height="100px">
-        Hello
+      <Flex
+        justify="center"
+        align="center"
+        height="100px"
+        borderBottom="white 1px solid"
+      >
+        <NextImage src="/logo.svg" height={60} width={120} />
       </Flex>
       <Flex justify="center" align="center" height="calc(100vh - 100px)">
-        form
+        <Box padding="50px" bg="gray.900" borderRadius="6px">
+          <form onSubmit={handleSubmit}>
+            <Input
+              placeholder="email"
+              type="email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              placeholder="password"
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            {/* get additional hover code from Scott's project */}
+            <Button type="submit" bg="green.500" isLoading={isLoading}>
+              {mode}
+            </Button>
+          </form>
+        </Box>
       </Flex>
     </Box>
   );
