98d2e719ea5f60fbd70358ef1ed589f9360cbaf7
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-12-15 11:05:00 -0600

    Initial player controls

---
+++ components/player.tsx
@@ -0,0 +1,30 @@
+import {
+  ButtonGroup,
+  Box,
+  IconButton,
+  RangeSlider,
+  RangeSliderFilledTrack,
+  RangeSliderTrack,
+  RangeSliderThumb,
+  Center,
+  Flex,
+  Text,
+} from "@chakra-ui/react";
+import React from "react";
+import ReactHowler from "react-howler";
+import { useEffect, useRef, useState } from "react";
+import {
+  MdShuffle,
+  MdSkipPrevious,
+  MdSkipNext,
+  MdOutlinePlayCircleFilled,
+  MdOutlinePauseCircleFilled,
+  MdOutlineRepeat,
+} from "react-icons/md";
+import { useStoreActions } from "easy-peasy";
+
+const Player = () => {
+  return <Box>Yo</Box>;
+};
+
+export default Player;
